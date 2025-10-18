e5675f0a90f44ea45cefe0a63a22c730a6d76bf5
Andydevs <akanshul97@gmail.com>
2025-10-18 19:54:24 -0400

    Add CSS support

--- examples/webpack-site/src/index.js
+++ examples/webpack-site/src/index.js
@@ -1,6 +1,7 @@
 const React = require("react")
 const ReactDOM = require("react-dom/client")
 import { App } from "./app/index.jsx"
+import "./styles.css"
 
 const element = document.getElementById("app")
 const root = ReactDOM.createRoot(element)
